d657ca5083a5866c684577a207b7615b251cc6d3
Sam Hartley <sam@jamtartley.com>
2024-05-12 13:46:24 +0100

    chore(term): dumbly putting chars into RenderBuffer

--- wangerz-client-term/src/main.rs
+++ wangerz-client-term/src/main.rs
@@ -1,6 +1,8 @@
 #![allow(dead_code)]
 
-use crossterm::{event, style, terminal};
+use std::io;
+
+use crossterm::{cursor, event, style, terminal, ExecutableCommand, QueueableCommand};
 
 #[derive(Clone, Debug)]
 enum CellStyle {
@@ -39,14 +41,63 @@ impl RenderBuffer {
 
         Self { cells }
     }
+
+    fn put_at(&mut self, i: usize, ch: char) {
+        self.cells[i].ch = ch;
+    }
+
+    fn render(&self, stdout: &mut io::Stdout) -> anyhow::Result<()> {
+        let width = 20;
+
+        for (i, cell) in self.cells.iter().enumerate() {
+            let (row, col) = ((i / width) as u16, (i % width) as u16);
+
+            stdout
+                .queue(cursor::MoveTo(col, row))?
+                .queue(style::Print(cell.ch))?;
+        }
+
+        Ok(())
+    }
+}
+
+trait Renderable {
+    fn render_into(&self, buf: &mut RenderBuffer, start: usize);
+}
+
+#[derive(Debug)]
+struct Prompt {
+    curr: Vec<char>,
+}
+
+impl Prompt {
+    fn new() -> Self {
+        Self { curr: vec![] }
+    }
+
+    fn append(&mut self, ch: char) {
+        self.curr.push(ch);
+    }
+}
+
+impl Renderable for Prompt {
+    fn render_into(&self, buf: &mut RenderBuffer, start: usize) {
+        for (i, &ch) in self.curr.iter().enumerate() {
+            buf.put_at(start + i, ch)
+        }
+    }
 }
 
 fn main() -> anyhow::Result<()> {
     let size = terminal::size()?;
-    let buf = RenderBuffer::new(size.0 as usize * size.1 as usize);
-    println!("{buf:?}");
+    let mut buf = RenderBuffer::new(size.0 as usize * size.1 as usize);
+    let mut prompt = Prompt::new();
+    let mut stdout = io::stdout();
 
     terminal::enable_raw_mode()?;
+    stdout
+        .execute(terminal::EnterAlternateScreen)?
+        .execute(terminal::DisableLineWrap)?;
 
     loop {
         let event = event::read()?;
@@ -54,12 +105,18 @@ fn main() -> anyhow::Result<()> {
         match event {
             event::Event::Key(event::KeyEvent { code, .. }) => match code {
                 event::KeyCode::Esc => break,
+                event::KeyCode::Char(ch) => prompt.append(ch),
                 _ => (),
             },
             _ => (),
         }
+
+        prompt.render_into(&mut buf, 0);
+
+        let _ = buf.render(&mut stdout)?;
     }
 
+    stdout.execute(terminal::LeaveAlternateScreen)?;
     terminal::disable_raw_mode()?;
 
     Ok(())
