6dc32d779afc6a817d8fce8cd6cd61ab8fe198b7
Sam Hartley <sam@jamtartley.com>
2024-05-12 13:26:57 +0100

    chore(term): initialize empty RenderBuffer struct

--- wangerz-client-term/src/main.rs
+++ wangerz-client-term/src/main.rs
@@ -1,9 +1,50 @@
 #![allow(dead_code)]
 
-use crossterm::{event, terminal};
+use crossterm::{event, style, terminal};
+
+#[derive(Clone, Debug)]
+enum CellStyle {
+    Bold,
+    Italic,
+    Normal,
+}
+
+#[derive(Clone, Debug)]
+struct RenderCell {
+    ch: char,
+    fg: style::Color,
+    bg: style::Color,
+    cell_style: CellStyle,
+}
+
+impl Default for RenderCell {
+    fn default() -> Self {
+        Self {
+            ch: ' ',
+            fg: style::Color::White,
+            bg: style::Color::Black,
+            cell_style: CellStyle::Normal,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct RenderBuffer {
+    cells: Vec<RenderCell>,
+}
+
+impl RenderBuffer {
+    fn new(size: usize) -> Self {
+        let cells = vec![RenderCell::default(); size];
+
+        Self { cells }
+    }
+}
 
 fn main() -> anyhow::Result<()> {
-    let _size = terminal::size;
+    let size = terminal::size()?;
+    let buf = RenderBuffer::new(size.0 as usize * size.1 as usize);
+    println!("{buf:?}");
 
     terminal::enable_raw_mode()?;
 
